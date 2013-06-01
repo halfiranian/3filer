c7eb293a71c582dd3ac5c7890e7bb66ff0c098e4
Halfiranian <jamessadri@gmail.com>
2013-06-01 21:13:55 +0100

    Local files pointing at BSD

--- files/scripts.js
+++ files/scripts.js
@@ -1,5 +1,21 @@
 $(function () {  // ignore this line, you need it wherever you use jQuery, it closes on the last line
-
+$("#multi .btn").click(function(){
+	var email = $("input#email").val();
+	var dataString = 'email=' + email;
+	console.log(dataString);
+	$.ajax({
+      type: "POST",
+      url: "http://action.greenpeace.org.uk/page/signup/multistep",
+      data: dataString,
+      success: function() {
+        $('#result').text("whoopwhoop")
+      }
+     });
+    return false;
+});
+	
+	
+	
 
 
 }); //closing jQuery opener here
